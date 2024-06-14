1e48d8ecb9ab72f4a16ba33035e2b57297c02e63
Morais Junior <geekdoagrest@gmail.com>
2024-06-14 19:27:33 -0300

    Start project

---
+++ src/test/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Test
+ * Description: Test transients and widget
+ * Author: Morais Junior
+ * Author URI: https://github.com/worais/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define('PLUGIN_DIR', dirname( __FILE__ ));
+
+class Plugin{
+
+}
